90a8be4baf6c3c957533b7001d5bef224941c81e
haoxle <hao-le@live.co.uk>
2022-10-21 17:12:23 +0100

    checkbox

---
+++ punkapi/src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,7 @@
+import "./Checkbox.scss";
+const Checkbox = () => {
+  // const { handleInput, searchTerm } = props;
+  return <input type="checkbox" />;
+};
+
+export default Checkbox;
--- punkapi/src/container/Nav/Nav.jsx
+++ punkapi/src/container/Nav/Nav.jsx
@@ -1,9 +1,28 @@
 import "./Nav.scss";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import Checkbox from "../../components/Checkbox/Checkbox";
 
 const Nav = (props) => {
   const { handleInput, searchTerm } = props;
-  return <SearchBar handleInput={handleInput} searchTerm={searchTerm} />;
+  return (
+    <div className="navigation">
+      <SearchBar handleInput={handleInput} searchTerm={searchTerm} />
+      <div className="checkbox">
+        <label className="checkbox__ABV">
+          {"High ABV > 6.0%"}
+          <Checkbox />
+        </label>
+        <label>
+          Classic
+          <Checkbox />
+        </label>
+        <label>
+          {"Acidic > pH 4"}
+          <Checkbox />
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
