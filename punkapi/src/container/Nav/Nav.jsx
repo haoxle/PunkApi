c53515c922d13511878898145274a6f59cbf7917
haoxle <hao-le@live.co.uk>
2022-10-24 23:11:32 +0100

    nav bar semidone

--- punkapi/src/container/Nav/Nav.jsx
+++ punkapi/src/container/Nav/Nav.jsx
@@ -1,24 +1,41 @@
 import "./Nav.scss";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import brewdog from "../../images/brewdog.png";
 
 const Nav = ({ handleInput, searchTerm, handleCheckBox }) => {
   return (
     <div className="navigation">
-      <SearchBar handleInput={handleInput} searchTerm={searchTerm} />
       <div className="checkbox">
-        <label className="checkbox__ABV">
-          {"High ABV > 6.0%"}
-          <input type="checkbox" value="ABV" onChange={handleCheckBox} />
-        </label>
-        <label>
-          Classic
-          <input type="checkbox" value="classic" onChange={handleCheckBox} />
-        </label>
-        <label>
-          {"Acidic > pH 4"}
-          <input type="checkbox" value="ph" onChange={handleCheckBox} />
-        </label>
+        <div className="checkbox__ABV">
+          <label className="checkbox__ABV--label">{"Alcohol > 6.0%"}</label>
+          <input
+            className="checkbox__ABV--check"
+            type="checkbox"
+            value="ABV"
+            onChange={handleCheckBox}
+          />
+        </div>
+        <div className="checkbox__Classic">
+          <label className="checkbox__Classic--label">Classic</label>
+          <input
+            className="checkbox__Classic--check"
+            type="checkbox"
+            value="classic"
+            onChange={handleCheckBox}
+          />
+        </div>
+        <div className="checkbox__Ph">
+          <label className="checkbox__Classic--label">{"Acidic > pH 4"}</label>
+          <input
+            className="checkbox__Classic--check"
+            type="checkbox"
+            value="ph"
+            onChange={handleCheckBox}
+          />
+        </div>
       </div>
+      <img className="navigation__logo" src={brewdog} alt="logo" />
+      <SearchBar handleInput={handleInput} searchTerm={searchTerm} />
     </div>
   );
 };
