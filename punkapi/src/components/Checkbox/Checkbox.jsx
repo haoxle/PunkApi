05718cb37d189f7d61658c98149d4be4f823311f
haoxle <hao-le@live.co.uk>
2022-10-24 21:18:00 +0100

    deleted checkbox

--- punkapi/src/components/Checkbox/Checkbox.jsx
+++
@@ -1,7 +0,0 @@
-import "./Checkbox.scss";
-const Checkbox = ({ ABV, getABV }) => {
-  // const { handleInput, searchTerm } = props;
-  return <input type="checkbox" value={ABV} onClick={getABV} />;
-};
-
-export default Checkbox;
