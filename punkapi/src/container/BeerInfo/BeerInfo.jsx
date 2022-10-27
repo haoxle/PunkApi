51d7231b45de1f7eaa241715769a3cbf1980da56
Hao Le <hao-le@live.co.uk>
2022-10-27 17:31:38 +0100

    foodpairing

--- punkapi/src/container/BeerInfo/BeerInfo.jsx
+++ punkapi/src/container/BeerInfo/BeerInfo.jsx
@@ -5,11 +5,8 @@ import HomeBtn from "../../components/HomeBtn/HomeBtn";
 const BeerInfo = ({ beers }) => {
   const { beerId } = useParams();
   const currentBeer = beers.filter((beer) => beer.id === parseFloat(beerId));
-
   const { image_url, name, description, ph, abv, first_brewed, food_pairing } =
     currentBeer[0];
-
-  console.log(currentBeer);
   return (
     <div className="info">
       <img className="info-img" src={image_url} alt={name} />
@@ -33,7 +30,7 @@ const BeerInfo = ({ beers }) => {
         </p>
         <p>
           <span className="info-content__title">Goes well with: </span>
-          {food_pairing}
+          {food_pairing.join(", ")}
         </p>
         <div className="home-button">
           <Link to="/">
