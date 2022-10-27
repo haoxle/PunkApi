f1b3ebe97c6eb8cb1a075ff9393b03427f5a574c
Hao Le <hao-le@live.co.uk>
2022-10-27 17:08:59 +0100

    testing for nav

--- punkapi/src/App.test.js
+++ punkapi/src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+it("check if beer is on the page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.queryByText(/Changing the world, one glass at a time./i);
+  expect(text).toBeInTheDocument();
+  expect(text).toBeTruthy();
 });
